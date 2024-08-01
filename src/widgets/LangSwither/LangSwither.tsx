f62a1daaa3ec4a230e71663c7efd4ebbbec15a91
ThePlayger <denys.smyrnov.2006@gmail.com>
2024-08-01 20:48:54 +0300

    Fix

--- src/widgets/LangSwither/LangSwither.tsx
+++ src/widgets/LangSwither/LangSwither.tsx
@@ -1,5 +1,4 @@
 import { classNames } from "shared/lib/className/classNames";
-import cls from "./LangSwither.module.scss";
 import { Button, ThemeButton } from "shared/ui/Button/Button";
 import { useTranslation } from "react-i18next";
 
@@ -14,7 +13,7 @@ export const LangSwither = ({ className }: LangSwitherProps) => {
     };
     return (
         <Button
-            className={classNames(cls.LangSwither, {}, [className])}
+            className={classNames("", {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
