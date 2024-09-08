d2e5e9df9138da22d8ff33c29e1bb42aea1e873f
ThePlayger <denys.smyrnov.2006@gmail.com>
2024-09-08 18:54:47 +0300

    StoryBook

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
-import { buildOptions } from "./types/config";
+import { buildOptions } from "./types/config";  
+import { buildCssLoader } from "./loadres/BuildCssLoader";
 
 export function buildLoaders({ isDev }: buildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -16,25 +16,8 @@ export function buildLoaders({ isDev }: buildOptions): webpack.RuleSetRule[] {
             },
         ],
     };
-
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => resPath.includes(".module"),
-                        localIdentName: isDev
-                            ? "[path][name]__[local]--[hash:base64:5]"
-                            : "[hash:base64:5]",
-                    },
-                },
-            },
-            "sass-loader",
-        ],
-    };
+    
+    const cssLoader = buildCssLoader(isDev)
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: "ts-loader",
